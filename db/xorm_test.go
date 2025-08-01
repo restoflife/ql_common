41dcd9b758daa7ec477d357f3b58dd95480b4125
dengquanguo <dengquanguo@libii.com>
2025-08-01 14:44:38 +0800

    init

--- db/xorm_test.go
+++ db/xorm_test.go
@@ -9,6 +9,7 @@ package db
 
 import (
 	"context"
+	`fmt`
 	"testing"
 
 	"xorm.io/xorm"
@@ -17,12 +18,13 @@ import (
 func TestName(t *testing.T) {
 	err := Transaction(context.Background(), "default", func(session *xorm.Session) error {
 		// 执行事务操作
-		if _, err := session.Insert(&user); err != nil {
+		if _, err := session.Insert(map[string]string{}); err != nil {
 			return err
 		}
-		if _, err := session.Update(&account); err != nil {
+		if _, err := session.Update(map[string]string{}); err != nil {
 			return err
 		}
 		return nil
 	})
+	fmt.Println(err)
 }
